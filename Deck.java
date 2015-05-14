2b492ccad5b8f68cbfe38e6338bca3ca6a9cbf06
dakedo <dakedo@users.noreply.github.com>
2015-05-13 22:46:53 -0700

    Update Deck.java

--- Deck.java
+++ Deck.java
@@ -5,42 +5,50 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Random;
 
+
 public class Deck
 {
     Queue<Card> cards;
+
     Random rand;
+
     int size;
-    
+
+
     /**
      * Creates the deck of cards.
-     * @param shuffled whether the deck starts out shuffled or not.
+     * 
+     * @param shuffled
+     *            whether the deck starts out shuffled or not.
      */
-    public Deck(boolean shuffled)
+    public Deck( boolean shuffled )
     {
         rand = new Random();
         cards = new LinkedList<Card>();
-        for (int j = 1; j <= 13; j++)
+        for ( int j = 1; j <= 13; j++ )
         {
-            cards.add( new Card(j, "Clubs") );
-            cards.add( new Card(j, "Diamonds") );
-            cards.add( new Card(j, "Hearts") );
-            cards.add( new Card(j, "Spades") );
+            cards.add( new Card( j, "Clubs" ) );
+            cards.add( new Card( j, "Diamonds" ) );
+            cards.add( new Card( j, "Hearts" ) );
+            cards.add( new Card( j, "Spades" ) );
             size += 4;
         }
-        if (shuffled)
+        if ( shuffled )
         {
             shuffle();
         }
     }
-    
+
+
     /**
      * no-param constructor. Defaults to a shuffled deck.
      */
     public Deck()
     {
-        this(true);
+        this( true );
     }
-    
+
+
     /**
      * Shuffles the deck of cards randomly.
      */
@@ -48,46 +56,58 @@ public class Deck
     {
         ArrayList<Card> array = new ArrayList<Card>();
         int sizeArchive = cards.size();
-        for (int j = 0; j < sizeArchive; j++)
+        for ( int j = 0; j < sizeArchive; j++ )
         {
             array.add( cards.poll() );
         }
-        for (int j = 0; j < sizeArchive; j++)
+        for ( int j = 0; j < sizeArchive; j++ )
         {
             cards.add( array.remove( rand.nextInt( array.size() ) ) );
         }
     }
-    
+
+
     /**
      * Draws the next card from the deck, removing it.
+     * 
      * @return the next card.
      */
     public Card draw()
     {
+        size--;
         return cards.poll();
     }
-    
+
+
     /**
      * Takes a card and puts it at the bottom of the deck.
-     * @param c the card collected.
+     * 
+     * @param c
+     *            the card collected.
      */
-    public void collect(Card c)
+    public void collect( Card c )
     {
         cards.add( c );
+        size++;
     }
-    
+
+
     /**
-     * Finds a card from the deck and draws it, replacing the deck how it was before.
-     * @param val value of the target card
-     * @return the target card, or a card with a -1 value and null suit if the card isn't found.
+     * Finds a card from the deck and draws it, replacing the deck how it was
+     * before.
+     * 
+     * @param val
+     *            value of the target card
+     * @return the target card, or a card with a -1 value and null suit if the
+     *         card isn't found.
      */
-    public Card find(int val)
+    public Card find( int val )
     {
         boolean check = true;
-        Card target = new Card(-1, "null");
-        for (int j = 0; j < cards.size(); j++)
+        Card target = new Card( -1, "null" );
+        for ( int j = 0; j < cards.size(); j++ )
         {
-            if (cards.peek().getValue() == val && check)
+            if ( cards.peek().getValue() == val && check )
             {
                 target = draw();
                 j++;
@@ -97,11 +117,18 @@ public class Deck
         }
         return target;
     }
-    
+
+
+    public int getSize()
+    {
+        return size;
+    }
+
+
     public String toString()
     {
         String result = "";
-        for (int j = 0; j < cards.size(); j++)
+        for ( int j = 0; j < cards.size(); j++ )
         {
             result += cards.peek().toString() + "\n";
             cards.add( cards.poll() );
@@ -109,5 +136,4 @@ public class Deck
         return result;
     }
 
-    
 }
