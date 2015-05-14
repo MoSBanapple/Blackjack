633a91ab3234808393f110bff4f73b0469c4bc3d
dakedo <dakedo@users.noreply.github.com>
2015-05-13 22:46:32 -0700

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,29 +1,34 @@
 package aldz_Blackjack;
 
-
 public class Card
 {
     private int value;
+
     private String suit;
-    
+
+
     /**
-     * @param val The number of the card. 11-13 represent royals.
-     * @param cardSuit The suit of the card.
+     * @param val
+     *            The number of the card. 11-13 represent royals.
+     * @param cardSuit
+     *            The suit of the card.
      */
-    public Card(int val, String cardSuit)
+    public Card( int val, String cardSuit )
     {
         value = val;
         suit = cardSuit;
     }
-    
+
+
     public int getValue()
     {
         return value;
     }
-    
+
+
     public int getBlackValue()
     {
-        if (value > 10)
+        if ( value > 10 )
         {
             return 10;
         }
@@ -32,21 +37,23 @@ public class Card
             return value;
         }
     }
-    
+
+
     public String getSuit()
     {
         return suit;
     }
-    
+
+
     public String toString()
     {
-        if (value > 1 && value < 11)
+        if ( value > 1 && value < 11 )
         {
             return value + " of " + suit;
         }
         else
         {
-            if (value == 1)
+            if ( value == 1 )
             {
                 return "Ace of " + suit;
             }
