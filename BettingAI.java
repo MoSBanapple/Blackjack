5b5de71ee33f06e76ad5430625833dc50df30cfd
dakedo <dakedo@users.noreply.github.com>
2015-05-18 13:57:08 -0700

    Update BettingAI.java

--- BettingAI.java
+++ BettingAI.java
@@ -16,6 +16,7 @@ public class BettingAI
 
     private int zen;
 
+
     public BettingAI()
     {
         hiLo = 0;
@@ -134,6 +135,97 @@ public class BettingAI
             red -= 1;
             zen -= 1;
         }
+    }
+
+
+    /**
+     * 
+     * TODO Write your method description here.
+     * 
+     * Skill levels:
+     * 
+     * -1 Drunkard
+     * 
+     * 0 Average
+     * 
+     * 1 Amateur
+     * 
+     * 2 Pro
+     * 
+     * @param p
+     * @param skill
+     * @param deck
+     * @return
+     */
+    int calculateBet( Player p, int skill, Deck deck )
+    {
+        int cash = p.sumChips();
+        double d = Math.random();
+
+        double score = 0;
+
+        if ( skill == -1 )
+        {
+            d = -1;
+        }
+        else if ( skill == 0 )
+        {
+            d *= 2;
+        }
+        else if ( skill == 1 )
+        {
+            d = 3 * d + 1;
+        }
+        else
+        {
+            // if(skill == 2)
+            d = 4 * d + 4;
+        }
+        int res = (int)d;
+
+        if ( res == -1 )
+        {
+            score = -hiLo;
+        }
+        else if ( res == 0 )
+        {
+            score = 0;
+        }
+        else if ( res == 1 )
+        {
+            score = hiLo;
+        }
+        else if ( res == 2 )
+        {
+            score = hiOptI;
+        }
+        else if ( res == 3 )
+        {
+            score = hiOptII;
+        }
+        else if ( res == 4 )
+        {
+            score = ko;
+        }
+        else if ( res == 5 )
+        {
+            score = omegaII;
+        }
+        else if ( res == 6 )
+        {
+            score = red;
+        }
+        else if ( res == 7 )
+        {
+            score = zen;
+        }
+
+        score /= deck.getSize();
+
+        int porportionToBet = (int)( ( score / 2 + 10 ) / 100 );
+        int betAmount = cash * porportionToBet;
+
+        return betAmount;
     }
 
 
